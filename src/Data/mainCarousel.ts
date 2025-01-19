942ddb6b3a3e61e0abc0b7980a0e3e0dcfd94893
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-01-19 23:56:03 +0530

    change main carousel image urls

--- src/Data/mainCarousel.ts
+++ src/Data/mainCarousel.ts
@@ -1,25 +1,27 @@
 export const homeCarouselData = [
   {
     image:
-      'https://i.ibb.co/5xzMkWp/548x669-Common-Printed-Tshirt-Banners-1736877528.webp',
+      'https://ik.imagekit.io/nltlgdoxs/548x669-Common-Printed-Tshirt-Banners-1736877528.webp?updatedAt=1737310282765',
     path: '/products/men/men_oversized_tshirts',
   },
   {
-    image: 'https://i.ibb.co/V2Yh1tn/548x669-BF-tee-women-1736601616.webp',
+    image:
+      'https://ik.imagekit.io/nltlgdoxs/548x669-BF-tee-women-1736601616.webp?updatedAt=1737310282570',
     path: '/products/women/women_tops',
   },
   {
     image:
-      'https://i.ibb.co/sQs2Xrp/548x669-common-Joggers-Banners-1-1736877561.webp',
+      'https://ik.imagekit.io/nltlgdoxs/548x669-common-Joggers-Banners-1-1736877561.webp?updatedAt=1737310282520',
     path: '/products/women/women_cargos',
   },
   {
     image:
-      'https://i.ibb.co/rQJ1Fpq/548x669-Common-Plusisze-WW-Banners-1736877530.webp',
+      'https://ik.imagekit.io/nltlgdoxs/548x669-Common-Plusisze-WW-Banners-1736877530%20(1).webp?updatedAt=1737310282187',
     path: '/products/men/men_sweaters',
   },
   {
-    image: 'https://i.ibb.co/ch0CyDr/548x669-Banners-12-1736877531.webp',
+    image:
+      'https://ik.imagekit.io/nltlgdoxs/548x669-Banners-12-1736877531.webp?updatedAt=1737310282763',
     path: '/products/men/men_cargos',
   },
 ];
